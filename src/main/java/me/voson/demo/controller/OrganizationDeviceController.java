b6193b404d03b0376280eaaeaa15aa31fb84c411
voson@ricnsmart.com <voson@2018>
2020-05-30 19:45:41 +0800

    引入slf4j日志框架

--- src/main/java/me/voson/demo/controller/OrganizationDeviceController.java
+++ src/main/java/me/voson/demo/controller/OrganizationDeviceController.java
@@ -3,23 +3,24 @@ package me.voson.demo.controller;
 import me.voson.demo.CommonResponse;
 import me.voson.demo.model.Organization;
 import me.voson.demo.service.OrganizationDeviceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toList;
 
 /**
  * @author voson
  */
 @RestController
 public class OrganizationDeviceController {
+    protected Logger logger = LoggerFactory.getLogger(OrganizationDeviceController.class);
+
     final OrganizationDeviceService organizationDeviceService;
 
     @Autowired
@@ -29,6 +30,7 @@ public class OrganizationDeviceController {
 
     @GetMapping("/device/organizations/{sn}")
     public Mono<CommonResponse<List<Organization>>> getDeviceSetting(@PathVariable() String sn) {
+        logger.info(sn);
         return organizationDeviceService.findOrganizationBySn(sn).collectList().map(CommonResponse::success);
     }
 }
